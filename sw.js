cf0d637725860b3e6e0cd270e2cad40d5742d89f
shail128 <shail1282003@gmail.com>
2026-02-12 20:37:40 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+// sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(list => {
+      for (const c of list) if ('focus' in c) return c.focus();
+      return clients.openWindow('./index.html');
+    })
+  );
+});
